62b4bc244856c29096cfd173fd66885c9d2b7183
Reggie Torres <reggietorres@proton.me>
2024-01-31 23:14:59 -0500

    add web

---
+++ web.py
@@ -0,0 +1,59 @@
+#First Party
+from functools import wraps
+
+#Third Party
+
+from flask import Flask, render_template, request, session, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+
+
+
+app = Flask(__name__, template_folder='templates')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://server:3865@localhost/server'
+db = SQLAlchemy(app)
+app.secret_key = 'your_secret_key' # PLEASE PLEASE GENERATE A EXTREMELY LONG PASSWORD HERE!
+
+class users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+    password = db.Column(db.String(50))
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'logged_in' not in session:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+@app.route('/')
+@login_required
+def home():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = users.query.filter_by(username=username).first()
+        if user and user.password == password:
+            session['logged_in'] = True
+            return redirect(url_for('home'))
+        else:
+            return 'Invalid credentials', 401
+    else:
+        return render_template('login.html')
+    
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('login'))    
+
+if __name__ == '__main__':
+    app.run(debug=True)
