9a938bab32846421337dd77f8ceade188cb18834
Reggie Torres <156597761+reggietorres@users.noreply.github.com>
2024-02-04 16:33:07 -0500

    Webserver | login.html now uses logos, favicon added  (#28)
    * add web
    
    * edit main.css, change footer1-2 to footer-left and footer-right
    
    * Add to the readme, how to test the webserver
    
    * In MariaDB, you cannot name a database "database", slight oversight..
    
    * Forgot to tell the user to "use asmara;" after creating the database..
    
    * 2fa
    
    * remove unused imports
    
    * add pytz
    
    * edit webhook system, add timeout,
    
    * add notes
    
    * login.html now uses logos, favicon added
    
    * revamp idle system
    
    * change titles
    
    * Change timeout to 30 seconds, automate secret key generation, remove discord 2fa for future system

--- web.py
+++ web.py
@@ -1,18 +1,34 @@
 #Standard Lib
-import random
+#import random
+import os
 from functools import wraps
-from datetime import datetime, timedelta
+#from datetime import datetime, timedelta
 
 #Third Party
 
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
-from pytz import utc
-from discord_webhook import DiscordWebhook, DiscordEmbed
+#from pytz import utc
+#from discord_webhook import DiscordWebhook, DiscordEmbed
+
+def load_or_generate_secret_key(filename='secret_key.txt'):
+    try:
+        with open(filename, 'r') as file:
+            secret_key = file.read().strip()
+            # Check if the file is empty
+            if not secret_key:
+                raise FileNotFoundError
+    except FileNotFoundError:
+        # Generate a new 128-character secret key
+        secret_key = os.urandom(128).hex()
+        with open(filename, 'w') as file:
+            file.write(secret_key)
+    return secret_key
 
 
+#Discord webhook functionality cut for better system in the future
 #Here we define the webhook
-webhook = DiscordWebhook(url="webhook here", username="ASMARA 2FA", avatar_url="https://avatars.githubusercontent.com/u/20572623?v=4")
+#webhook = DiscordWebhook(url="webhook here", username="ASMARA 2FA", avatar_url="https://github.com/A-c0rN/ASMARA/blob/main/assets/asmara-logo-only.png?raw=true")
 
 
 
@@ -20,7 +36,15 @@ webhook = DiscordWebhook(url="webhook here", username="ASMARA 2FA", avatar_url="
 app = Flask(__name__, template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/asmara' #Change this!
 db = SQLAlchemy(app)
-app.secret_key = 'your_secret_key' # PLEASE PLEASE GENERATE A EXTREMELY LONG PASSWORD HERE!
+
+
+# Load or generate the secret key
+app.secret_key = load_or_generate_secret_key()
+
+
+if app.secret_key == 'your_secret_key':
+    new_secret_key = os.urandom(128)
+    print(new_secret_key)
 
 #Make Database Model
 class users(db.Model):
@@ -37,6 +61,15 @@ def login_required(f):
             return redirect(url_for('login'))
         return f(*args, **kwargs)
     return decorated_function
+
+
+@app.route('/assets/<path:filename>')
+def serve_public_file(filename):
+    return send_from_directory('assets', filename)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory('assets', 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 #index.html route
 @app.route('/')
@@ -55,14 +88,15 @@ def login():
         user = users.query.filter_by(username=username).first()
         if user and user.password == password:
             # Generate a six-digit random code
-            code = str(random.randint(100000, 999999))
+            #code = str(random.randint(100000, 999999))
             # Send this code to a specific channel in your Discord server
-            send_discord_message(code)
+            #send_discord_message(code)
             # Store the code and its expiration time in the session
-            session['code'] = {'value': code, 'expires_at': datetime.now(utc) + timedelta(seconds=30)}
+            #session['code'] = {'value': code, 'expires_at': datetime.now(utc) + timedelta(seconds=30)}
             # Store the username in the session
+            session['logged_in'] = True
             session['username'] = username
-            return redirect(url_for('verify_code'))
+            return redirect(url_for('home'))
         else:
             return 'Invalid credentials', 401
     else:
@@ -74,27 +108,27 @@ def extend_session():
     return '', 204
     
 #2FA Route
-@app.route('/verify_code', methods=['GET', 'POST'])
-def verify_code():
+#@app.route('/verify_code', methods=['GET', 'POST'])
+#def verify_code():
     # Check if the code exists in the session
-    if 'code' not in session:
-        return '401 Unauthorized, Invalid Code, or you are trying to hack me!', 401
+#    if 'code' not in session:
+#        return '401 Unauthorized, Invalid Code, or you are trying to hack me!', 401
 
-    if request.method == 'POST':
-        entered_code = request.form.get('code')
-        code_info = session.get('code')
-        if code_info and entered_code == code_info['value'] and datetime.now(utc) <= code_info['expires_at']:
-            session['logged_in'] = True
-            # Clear the session variable holding the verification code
-            session.pop('code', None)
-            return redirect(url_for('home'))
-        else:
-            # Clear the session variable holding the verification code
-            session.pop('code', None)
-            return redirect(url_for('verify_code'))
-    else:
-        # Retrieve the flashed message from the session and store it in the template context
-        return render_template('verify_code.html')
+#    if request.method == 'POST':
+#        entered_code = request.form.get('code')
+#        code_info = session.get('code')
+#        if code_info and entered_code == code_info['value'] and datetime.now(utc) <= code_info['expires_at']:
+#            session['logged_in'] = True
+#            # Clear the session variable holding the verification code
+#            session.pop('code', None)
+#            return redirect(url_for('home'))
+#        else:
+#            # Clear the session variable holding the verification code
+#            session.pop('code', None)
+#            return redirect(url_for('verify_code'))
+#    else:
+#        # Retrieve the flashed message from the session and store it in the template context
+#        return render_template('verify_code.html')
 
 
 #Logout Route
@@ -105,13 +139,13 @@ def logout():
 
 
 #Discord 2FA Embed
-def send_discord_message(content):
-  embed = DiscordEmbed(title="2FA Code Requested", description="Code: " + str(content) + " If you didn't request this code, its safe to ignore this message..", color="03b2f8")
-  embed.set_author(name="ASMARA")
-  embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/907457287864590396/907757517751332914/warning.png")
-  embed.set_footer(text="Code Invalid After 30 Seconds | ASMARA © 2024 MSNGTXTRS SOFT.")
-  webhook.add_embed(embed)
-  webhook.execute()
+#def send_discord_message(content):
+#  embed = DiscordEmbed(title="2FA Code Requested", description="Code: " + str(content) + " If you didn't request this code, its safe to ignore this message..", color="03b2f8")
+#  embed.set_author(name="ASMARA")
+#  embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/907457287864590396/907757517751332914/warning.png")
+#  embed.set_footer(text="Code Invalid After 30 Seconds | ASMARA © 2024 MSNGTXTRS SOFT.")
+#  webhook.add_embed(embed)
+#  webhook.execute()
 
 if __name__ == '__main__':
     app.run(debug=True)
